4c1b9ad28120a19af082285882cc249c18912e0f
EmbyrCelyca <ValerieKnox7@gmail.com>
2023-10-01 17:22:37 -0400

    Created default hotkeys and integrate with current skill system

--- ValorbornARPG_v6/hotkey_manager.py
+++ ValorbornARPG_v6/hotkey_manager.py
@@ -1,5 +1,6 @@
 # hotkey_manager.py
-
+import pygame
+from settings import DEFAULT_HOTKEYS
 # This module defines the HotkeyManager class, which manages the mapping of keys to skills.
 
 class HotkeyManager:
@@ -7,12 +8,32 @@ class HotkeyManager:
 # Initializes the HotkeyManager with a reference to the SkillsEngine and empty key mappings.
         print("HotkeyManager initialized")
         self.skills_engine = skills_engine
-        self.key_mapping = {}  # Mapping of keys to skill_keys
+        self.key_mapping = self.map_to_pygame_keys(DEFAULT_HOTKEYS)  # Mapping of keys to skill_keys
         self.key_states = {}  # Track the state of each key
     
     def update(self):
+        keys = pygame.key.get_pressed()
+        # Check only the keys that are mapped
+        for key in self.key_mapping.keys():
+            self.key_states[key] = keys[self.key_mapping[key]]
         pass  # Placeholder for the update method
         
+    
+    def map_to_pygame_keys(self, default_keys):
+        pygame_key_mapping = {
+            'space': pygame.K_SPACE,
+            'lmb': pygame.BUTTON_LEFT,
+            'rmb': pygame.BUTTON_RIGHT,
+            'a': pygame.K_a,
+            's': pygame.K_s,
+            'd': pygame.K_d,
+            'f': pygame.K_f,
+            'g': pygame.K_g,
+            'v': pygame.K_v,
+            'left_shift': pygame.K_LSHIFT,
+            'esc': pygame.K_ESCAPE
+            }
+        return {action: pygame_key_mapping[key] for action, key in default_keys.items()}
     def map_skill_keys(self, key_mapping):
 # Maps skill keys to keyboard keys.
         for key, skill_key in key_mapping.items():
--- ValorbornARPG_v6/settings.py
+++ ValorbornARPG_v6/settings.py
@@ -5,3 +5,18 @@ SCREEN_HEIGHT = 720
 # Frame rate
 FPS = 60
 alpha = 0.1  # Your alpha setting
+
+# Default hotkeys
+DEFAULT_HOTKEYS = {
+    'dash': 'space',
+    'skill_1': 'lmb',
+    'skill_2': 'rmb',
+    'skill_3': 'a',
+    'skill_4': 's',
+    'skill_5': 'd',
+    'skill_6': 'f',
+    'skill_7': 'g',
+    'skill_8': 'v',
+    'force_no_move': 'left_shift',
+    'open_menu': 'esc'
+    }
